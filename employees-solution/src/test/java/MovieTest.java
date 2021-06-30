18f9dfad8309aa94da5c98205ebc1f2588abef3a
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-06-30 07:39:37 +0200

    Bikes project

---
+++ employees-solution/src/test/java/MovieTest.java
@@ -0,0 +1,16 @@
+/*public class MovieTest {
+
+    Movie movie = new Movie("Titanic", 120);
+
+    @Test
+    void setTitleTest(){
+        movie.setTitle("Star Wars");
+
+        assertEquals("Star Wars", movie.getTitle());
+    }
+
+    @Test
+    void getTitleTest(){
+        assertEquals("Titanic", movie.getTile());
+    }
+}*/
